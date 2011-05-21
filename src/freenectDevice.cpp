0b4b6daf899e755a8b01c3ec6c3119fa7c7be528
Paul Labedan <plabedan@gmail.com>
2011-05-21 16:41:07 +0200

    separate data from a specific distance interval

--- src/freenectDevice.cpp
+++ src/freenectDevice.cpp
@@ -16,12 +16,37 @@ void FreenectDevice::DepthCallback(void* _depth, uint32_t timestamp)
 
   QMutexLocker Locker(&_depthMutex);
   uint16_t* depth = static_cast<uint16_t*>(_depth);
-  if (_reverseMode)
-    for( unsigned int i = 0 ; i < FREENECT_FRAME_PIX; ++i)
-      memset(&_bufferDepth[i<<2], 0xff - (_gamma[depth[i]]>>8 <= 1?_gamma[depth[i]]>>1:0xff), 3);
-  else
-    for( unsigned int i = 0 ; i < FREENECT_FRAME_PIX; ++i)
-      memset(&_bufferDepth[i<<2], (_gamma[depth[i]]>>8 <= 1?_gamma[depth[i]]>>1:0xff), 3);
+
+  unsigned char CMin = 0xff;
+  for( unsigned int i = 0 ; i < FREENECT_FRAME_PIX; ++i)
+  {
+    unsigned char C = (_gamma[depth[i]]>>8 <= 1?_gamma[depth[i]]>>1:0xff);
+
+    if ((_gamma[depth[i]]>>7) < 2 && C < CMin && C > 0)
+      CMin = C;
+  }
+
+  for( unsigned int i = 0 ; i < FREENECT_FRAME_PIX; ++i)
+  {
+    unsigned char C = (_gamma[depth[i]]>>8 <= 1?_gamma[depth[i]]>>1:0xff);
+
+    if ((_gamma[depth[i]]>>7) >= 2)
+    {
+      if (_reverseMode) C ^= 0xff;
+      memset(&_bufferDepth[i<<2], C, 3);
+    }
+    else
+    {
+      if (C-16 < CMin)
+      {
+        _bufferDepth[(i<<2) + 1] = 0;
+        _bufferDepth[(i<<2) + 2] = 0xff;
+      }
+      else
+        memset(&_bufferDepth[i<<2]+1, C, 2);
+      _bufferDepth[i<<2] = 0xff;
+    }
+  }
 
   _newDepthFrame = true;
 }
